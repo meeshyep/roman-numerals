a1b52ec66fb0f05f5cab4e3b6fa7a2c9ff74a0ad
Michelle Epstein <epstein.michelle@gmail.com>
2017-01-11 00:49:57 +0000

    Passing test: Converts 1 or 5 to 'I' or 'V'

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,7 +1,12 @@
 class RomanNumerals
     
-    def self.convert(number)
-        "I"
-    end
+    ROMAN_NUMERALS = {5 => "V", 1 => "I"}
     
+    def self.convert(number)
+        roman_numerals = ""
+        ROMAN_NUMERALS.each do |k,v|
+            (number/k).times {roman_numerals << v; number -= k}
+        end
+        roman_numerals
+    end
 end 
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -5,4 +5,8 @@ describe RomanNumerals do
     it 'should convert 1 to "I"' do 
         expect(RomanNumerals.convert(1)).to eq "I"
     end
+    
+    it 'should convert 5 to "V"' do 
+        expect(RomanNumerals.convert(5)).to eq "V"
+    end 
 end
